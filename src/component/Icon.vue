de612eefc9b5d06dbb5be2c71aee812f713b0aed
draganet <3417237276@qq.com>
2022-12-28 16:28:18 +0800

    svgIcon插件，Icon组件

---
+++ src/component/Icon.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+  import { computed, defineComponent } from 'vue';
+
+  defineComponent({ name: 'Icon' });
+
+  const props = defineProps({
+    name: {
+      type: String,
+      required: true,
+    },
+  });
+  const iconName = computed(() => `#icon-${props.name}`);
+
+</script>
+
+<template>
+  <svg class="icon">
+    <use :xlink:href="iconName"></use>
+  </svg>
+</template>
+
+<style lang="scss">
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
